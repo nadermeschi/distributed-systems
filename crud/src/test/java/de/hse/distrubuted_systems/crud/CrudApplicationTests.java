68def57c55d987fb38c0d3aec51653e9bf614830
nadermeschi <nadermeschi07@gmail.com>
2021-04-11 18:17:01 +0200

    Initialize new project (CRUD Controller)

---
+++ crud/src/test/java/de/hse/distrubuted_systems/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hse.distrubuted_systems.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
